ebe723ae05a32121ff0e56e77e5d031a656329de
zakzakst <e1433104@gmail.com>
2023-12-03 19:02:10 +0900

    live server

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,6 @@
 module.exports = (eleventyConfig) => {
   eleventyConfig.addWatchTarget("./src/scss/");
+  eleventyConfig.addWatchTarget("./src/js/");
   return {
     dir: {
       input: "src/pages",
